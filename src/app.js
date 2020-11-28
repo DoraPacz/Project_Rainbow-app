0b5f8d448696f0fd3194393f4eb1273ca47d7090
DoraPacz <74684947+DoraPacz@users.noreply.github.com>
2020-11-28 19:25:00 +0100

    Coding of icon element

--- src/app.js
+++ src/app.js
@@ -23,20 +23,21 @@ document.querySelector("#temperature").innerHTML=Math.round(response.data.main.t
 document.querySelector("#humidity").innerHTML=response.data.main.humidity;
 document.querySelector("#wind").innerHTML=Math.round(response.data.wind.speed);
 document.querySelector("#description").innerHTML=response.data.weather[0].main;
+let iconElement=document.querySelector("#icon");
+iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+ iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city){
 let apiKey="6607db0703fd733599009302c215256a";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeatherCondition);
-
 }
 
 function handleSubmit(event){
     event.preventDefault();
     let city=document.querySelector("#city-input").value;
     search(city);
-    
 }
 
 function convertToFahrenheit(event){
@@ -44,7 +45,6 @@ function convertToFahrenheit(event){
     let temperatureElement=document.querySelector("#temperature");
 temperatureElement.innerHTML=66;
 }
-
 
 function convertToCelsius(event){
     event.preventDefault();
@@ -63,7 +63,6 @@ let apiUrl=`https://api.openweathermap.org/data/2.5/weather?lat=${position.coord
 axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-
 let dateElement=document.querySelector ("#date");
 let currentTime= new Date();
 dateElement.innerHTML= formatDate(currentTime);
@@ -81,3 +80,4 @@ fahrenheitLink.addEventListener("click",convertToFahrenheit);
 
 let celsiusLink= document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
